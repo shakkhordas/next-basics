a577848eb2da291c760bd50a335e77a20eda3d21
shakkhordas <shakkhordas117@gmail.com>
2024-10-03 01:01:20 +0600

    learned about private folders and route groups

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,13 @@
+const PrivateRoute = () => {
+  return (
+    <div>
+        <h1>You cannot view this in the browser</h1>
+        
+    </div>
+  )
+}
+
+export default PrivateRoute
+
+// in order to keep _ in the route, you can prefix the route name with %5F
+
