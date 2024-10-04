5489c8b164881ab22fda71a6427bbeba23888ad3
shakkhordas <shakkhordas117@gmail.com>
2024-10-04 22:12:12 +0600

    layouts and components

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <div style={{
+        backgroundColor: "ghostwhite",
+        padding: "1rem"
+      }}>Footer</div>
+    )
+  }
+  
+  export default Footer
---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div style={{
+      backgroundColor: "lightblue",
+      padding: "1rem"
+    }}>Header</div>
+  )
+}
+
+export default Header
